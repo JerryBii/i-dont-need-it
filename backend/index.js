e1c2ff7c580d1954285f16fbf1c50ca42ba578cf
Jerry Bi <jerrybi1202@outlook.com>
2022-02-05 18:24:38 -0500

    Backend stuffs

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const login = require("./db/credentials");
 const db = require("./db");
 const axios = require("axios");
+const userRouter = require("./routes/user-routes");
 
 const app = express();
 const port = 3000;
@@ -14,22 +15,27 @@ const params = {
   url: "https://www.amazon.com/s/zgbs/pc/516866",
 };
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 app.get("/", (req, res) => {
-  console.log("Hello World!");
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
+app.use(userRouter);
+
 app.get("/api/getProductData", async (req, res) => {
   axios
     .get(
-      "https://api.rainforestapi.com/request?api_key=0991188F694E4C7AAEBF983FBE865355&type=product&asin=B000YDDF6O&amazon_domain=amazon.com"
+      "https://api.rainforestapi.com/request?api_key=0991188F694E4C7AAEBF983FBE865355&type=product&asin=B07PXGQC1Q&amazon_domain=amazon.ca"
     )
     .then((response) => {
-      console.log(JSON.stringify(response.data, 0, 2)); //Need to parse this properly later
+      console.log(JSON.stringify(response.data.product)); //Need to parse this properly later
     });
 });
