aafa85e4b6538d3b0a83507f58bd5fdc3e635ab1
Jerry Bi <jerrybi1202@outlook.com>
2022-02-05 18:48:43 -0500

    Backend stuffs (#10)

--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -3,9 +3,25 @@ const UserCtrl = require("../controllers/user-ctrl");
 
 const router = express.Router();
 
-router.post("/User/create", UserCtrl.createUser);
-router.put("/User/updateSettings", UserCtrl.updateUserSettings);
-router.put("/User/updatePurchases", UserCtrl.updatePurchases);
-router.put("/User/deleteUser", UserCtrl.deleteUser);
+router.post("/user/create", UserCtrl.createUser);
+router.get("/user/get", UserCtrl.getUser);
+router.patch("/user/update/weeklyspent", UserCtrl.updateWeeklySpent);
+router.patch("/user/update/monthlyspent", UserCtrl.updateMonthlySpent);
+router.patch("/user/update/monthlylimit", UserCtrl.updateMonthlyLimit);
+router.patch("/user/update/weeklylimit", UserCtrl.updateWeeklyLimit);
+router.patch("/user/update/totalproducts", UserCtrl.updateTotalProducts);
+router.patch(
+  "/user/update/weeklyitemsnotpurchased",
+  UserCtrl.updateWeeklyItemsNotPurchased
+);
+router.patch(
+  "/user/update/monthlyitemsnotpurchased",
+  UserCtrl.updateMonthlyItemsNotPurchased
+);
+router.patch("/user/update/monthlysaved", UserCtrl.updateMonthlySaved);
+router.patch("/user/update/weeklysaved", UserCtrl.updateWeeklySaved);
+router.patch("/user/update/purchases", UserCtrl.updatePurchases);
+router.patch("/user/update/avoidancelist", UserCtrl.updateAvoidanceList);
+router.delete("/user/delete", UserCtrl.deleteUser);
 
 module.exports = router;
